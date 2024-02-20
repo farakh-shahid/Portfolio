54361908cb079b4ebc19591789acdfbc97b4e8f5
farakh-shahid <farakhraja72@gmail.com>
2024-02-20 16:18:00 +0500

    separate nav items

---
+++ portfolio/src/constants/navItems.ts
@@ -0,0 +1,6 @@
+export const navItems = new Map([
+    ['about', { href: '#about-me', label: 'About me' }],
+    ['skills', { href: '#skills', label: 'Skills' }],
+    ['projects', { href: '#projects', label: 'Projects' }],
+    ['resume', { href: '/resume', label: 'Resume' }],
+  ]);
