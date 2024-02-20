efbcc0bd11d02147f695e7bd0092ca5a7574e31b
farakh-shahid <farakhraja72@gmail.com>
2024-02-20 16:16:52 +0500

    updated navbar

---
+++ portfolio/src/app/resume/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Page() {
+  return (
+    <>
+    <h1 className='bg-red text-red ml-30'>Resume</h1>
+    </>
+  )
+}
--- portfolio/src/components/main/Navbar.tsx
+++ portfolio/src/components/main/Navbar.tsx
@@ -1,8 +1,15 @@
 import { Socials } from "../../constants";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const Navbar = () => {
+  const navItems = new Map([
+    ['about', { href: '#about-me', label: 'About me' }],
+    ['skills', { href: '#skills', label: 'Skills' }],
+    ['projects', { href: '#projects', label: 'Projects' }],
+    ['resume', { href: '/resume', label: 'Resume' }],
+  ]);
   return (
     <div className="w-full h-[65px] fixed top-0 shadow-lg shadow-[#2A0E61]/50 bg-[#03001417] backdrop-blur-md z-50 px-10">
       <div className="w-full h-full flex flex-row items-center justify-between m-auto px-[10px]">
@@ -25,18 +32,15 @@ const Navbar = () => {
 
         <div className="w-[500px] h-full flex flex-row items-center justify-between md:mr-20">
           <div className="flex items-center justify-between w-full h-auto border border-[#7042f861] bg-[#0300145e] mr-[15px] px-[20px] py-[10px] rounded-full text-gray-200">
-            <a href="#about-me" className="cursor-pointer">
-              About me
+          {Array.from(navItems, ([key, value]) => (
+            <a
+              href={value.href}
+              className="cursor-pointer"
+              key={key}
+            >
+              {value.label}
             </a>
-            <a href="#skills" className="cursor-pointer">
-              Skills
-            </a>
-            <a href="#projects" className="cursor-pointer">
-              Projects
-            </a>
-            <a href="#projects" className="cursor-pointer">
-              Resume
-            </a>
+          ))}
           </div>
         </div>
 
