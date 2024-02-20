416a17b09fc1024bde49a21f1a36aab07aff6da7
farakh-shahid <farakhraja72@gmail.com>
2024-02-20 19:04:03 +0500

    set Navbar

--- portfolio/src/components/main/Navbar.tsx
+++ portfolio/src/components/main/Navbar.tsx
@@ -1,46 +1,47 @@
-import { navItems } from "@/src/constants/navItems";
-import { Socials } from "../../constants";
-import Image from "next/image";
-import Link from "next/link";
-import React from "react";
+import { navItems } from '@/src/constants/navItems';
+import { Socials } from '../../constants';
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
 
 const Navbar = () => {
- 
   return (
-    <div className="w-full h-[65px] fixed top-0 shadow-lg shadow-[#2A0E61]/50 bg-[#03001417] backdrop-blur-md z-50 px-10">
-      <div className="w-full h-full flex flex-row items-center justify-between m-auto px-[10px]">
+    <div className='w-full h-[65px] fixed top-0 shadow-lg shadow-[#2A0E61]/50 bg-[#03001417] backdrop-blur-md z-50 px-10'>
+      <div className='w-full h-full flex flex-row items-center justify-between m-auto px-[10px]'>
         <a
-          href="#about-me"
-          className="h-auto w-auto flex flex-row items-center"
+          href='#about-me'
+          className='h-auto w-auto flex flex-row items-center'
         >
-          <Image
-            src="/NavLogo.png"
+          {/* <Image
+            src="/avatar1.jpg"
             alt="logo"
             width={70}
             height={70}
             className="cursor-pointer hover:animate-slowspin"
+          /> */}
+
+          <img
+            className='cursor-pointer hover:animate-slowspin w-10 h-10 p-1 rounded-full ring-2 ring-[#b49bff] dark:ring-purple-500'
+            src='/avatar1.jpg'
+            alt='Bordered avatar'
           />
 
-          <span className="font-bold ml-[10px] hidden md:block text-gray-300">
+          <span className='font-bold ml-[10px] hidden md:block text-gray-300'>
             Farrukh
           </span>
         </a>
 
-        <div className="w-[500px] h-full flex flex-row items-center justify-between md:mr-20">
-          <div className="flex items-center justify-between w-full h-auto border border-[#7042f861] bg-[#0300145e] mr-[15px] px-[20px] py-[10px] rounded-full text-gray-200">
-          {Array.from(navItems, ([key, value]) => (
-            <a
-              href={value.href}
-              className="cursor-pointer"
-              key={key}
-            >
-              {value.label}
-            </a>
-          ))}
+        <div className='w-[500px] h-full flex flex-row items-center justify-between md:mr-20'>
+          <div className='flex items-center justify-between w-full h-auto border border-[#7042f861] bg-[#0300145e] mr-[15px] px-[20px] py-[10px] rounded-full text-gray-200'>
+            {Array.from(navItems, ([key, value]) => (
+              <a href={value.href} className='cursor-pointer' key={key}>
+                {value.label}
+              </a>
+            ))}
           </div>
         </div>
 
-        <div className="flex flex-row gap-5">
+        <div className='flex flex-row gap-5'>
           {Socials.map((social) => (
             <Image
               src={social.src}
