a5353caa0e28f98624b20d5f86c7ac822430df8e
farakh-shahid <farakhraja72@gmail.com>
2024-02-22 14:00:19 +0500

    add tailwind configuration and

--- portfolio/tailwind.config.ts
+++ portfolio/tailwind.config.ts
@@ -15,6 +15,16 @@ module.exports = {
       titleFont: ['Montserrat', 'sans-serif'],
     },
     extend: {
+      screens: {
+        xs: '320px',
+        sm: '375px',
+        sml: '500px',
+        md: '667px',
+        mdl: '768px',
+        lg: '960px',
+        lgl: '1024px',
+        xl: '1280px',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
@@ -25,6 +35,9 @@ module.exports = {
         lightText: '#c4cfde',
         boxBg: 'linear-gradient(145deg, #1e2024, #23272b)',
         designColor: '#ff014f',
+      },
+      boxShadow: {
+        shadowOne: '10px 10px 19px #1c1e22, -10px -10px 19px #262a2e',
       },
     },
   },
