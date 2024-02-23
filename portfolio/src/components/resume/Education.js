d55ac63722e15beb6eb1ef9c1a39f600fb7a2119
farakh-shahid <farakhraja72@gmail.com>
2024-02-23 18:47:18 +0500

    remove stale code

--- portfolio/src/components/resume/Education.js
+++ portfolio/src/components/resume/Education.js
@@ -10,7 +10,6 @@ const Education = () => {
       animate={{ opacity: 1, transition: { duration: 0.5 } }}
       className=' flex flex-col lgl:flex-row gap-10 lgl:gap-20'
     >
-      {/* part one */}
       <div>
         <div className='py-6 lgl:py-12 font-titleFont flex flex-col gap-4'>
           <p className='text-sm text-designColor tracking-[4px]'>2014 - 2022</p>
@@ -40,36 +39,6 @@ const Education = () => {
           />
         </div>
       </div>
-      {/* part Two */}
-
-      {/* <div>
-        <div className='py-6 lgl:py-12 font-titleFont flex flex-col gap-4'>
-          <p className='text-sm text-designColor tracking-[4px]'>2022</p>
-          <h2 className='text-3xl md:text-4xl font-bold text-[#C4CFDE]'>
-            Job Experience
-          </h2>
-        </div>
-        <div className='mt-6 lgl:mt-14 w-full h-[1000px] border-l-[6px] border-l-[#2a0c612b] border-opacity-30 flex flex-col gap-10'>
-          <ResumeCard
-            title='Sr. Software Engineer'
-            subTitle='Google Out Tech - (2017 - Present)'
-            result='USA'
-            des="Google's hiring process is an important part of our culture. Googlers care deeply about their teams and the people who make them up."
-          />
-          <ResumeCard
-            title='Web Developer & Trainer'
-            subTitle='Apple Developer Team - (2012 - 2016)'
-            result='MALAYSIA'
-            des="A popular destination with a growing number of highly qualified homegrown graduates, it's true that securing a role in Malaysia isn't easy."
-          />
-          <ResumeCard
-            title='Front-end Developer'
-            subTitle='Nike - (2020 - 2011)'
-            result='Oman'
-            des='The Oman economy has grown strongly over recent years, having transformed itself from a producer and innovation-based economy.'
-          />
-        </div>
-      </div> */}
     </motion.div>
   );
 };
